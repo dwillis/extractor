23dbd3c967addb462f312fa223a38907a57fbbe2
Derek Willis <dwillis@nytimes.com>
2015-01-11 00:28:01 -0500

    initial commit

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask import render_template, request, jsonify
+from flask_bootstrap import Bootstrap
+import eatiht.etv2 as etv2
+app = Flask(__name__)
+Bootstrap(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/result/', methods=['GET','POST'])
+def result():
+    url = request.form['url']
+    tree = etv2.extract(url)
+    text = tree.get_text()
+    return jsonify(url=url, text=text)
+
+if __name__ == '__main__':
+    app.run()
