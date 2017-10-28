b9119acf192883d4d3ba3318ae2850501627dcca
Derek Willis <dwillis@gmail.com>
2017-10-28 07:53:31 -0400

    python 3 version

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ from flask import Flask
 from flask import render_template, request, jsonify
 from flask_bootstrap import Bootstrap
 from flask_reggie import Reggie
-import eatiht.etv2 as etv2
+from libextract.api import extract
+from requests import get
 app = Flask(__name__)
 Bootstrap(app)
 Reggie(app)
@@ -14,18 +15,21 @@ def index():
 @app.route('/result/', methods=['POST'])
 def result():
     url = request.form['url']
-    tree = etv2.extract(url)
-    text = tree.get_text()
+    r = get(url)
+    textnodes = list(extract(r.content))
+    text = textnodes[3].text_content()
+    print(textnodes)
     api_url = url.replace("?","@").replace("&","%")
-    return render_template('result.html', url=unicode(url), text=text, api_url=api_url)
+    return render_template('result.html', url=url, text=text, api_url=api_url)
 
 @app.route('/api/<regex(".*$"):url>')
 def api(url):
     url = url.replace("@","?").replace("%","&")
-    print url
+    r = get(url)
     tree = etv2.extract(url)
-    text = tree.get_text()
-    return jsonify(url=unicode(url), text=text)
+    textnodes = list(extract(r.content))
+    text = textnodes[3].text_content()
+    return jsonify(url=url, text=text)
 
 if __name__ == '__main__':
     app.run()
